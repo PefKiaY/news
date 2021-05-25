ec0e037bf654abe90b1bed34db9f54386492adc5
PefKiaY <yang1353585659@163.com>
2021-05-25 22:54:20 +0800

    提取配置文件和创建app

--- manager.py
+++ manager.py
@@ -1,11 +1,18 @@
 """
 程序的启动入口
 
+加载配置文件：
+1.数据库配置
+2.redis配置
+3.session配置
+4.csrf配置
+5.迁移
+6.日志记录
+
 """
+from info import create_app
 
-from flask import Flask
-
-app = Flask(__name__)
+app = create_app()
 
 
 @app.route('/')
