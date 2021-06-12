f7935087f361cdf59850b6e52761b66813258a36
PefKiaY <yang1353585659@163.com>
2021-06-12 19:36:24 +0800

    完善配置信息

--- info/__init__.py
+++ info/__init__.py
@@ -1,12 +1,32 @@
+import redis
 from flask import Flask
+from flask.ext.session import Session
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.wtf import CSRFProtect
 
-from config import Config
+from config import config_dict
 
-def create_app():
+# 定义redis
+redis_store = None
+
+
+def create_app(env_name):
     app = Flask(__name__)
 
     # 创建数据库对象，关联app
     db = SQLAlchemy(app)
     # 加载配置信息
-    app.config.from_object(Config)
+    config = config_dict[env_name]
+    app.config.from_object(config)
+
+    # 创建redis对象
+    global redis_store
+    redis_store = redis.StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, decode_responses=True)
+
+    # 设置csrf对app进行保护
+    CSRFProtect(app)
+
+    # 初始化Session
+    Session(app)
+
+    return app
--- manager.py
+++ manager.py
@@ -12,7 +12,7 @@
 """
 from info import create_app
 
-app = create_app()
+app = create_app('develop')
 
 
 @app.route('/')
