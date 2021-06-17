7579a8fc6968b0b336aa70c8c43ffd0b5925250b
PefKiaY <yang1353585659@163.com>
2021-06-17 14:55:29 +0800

    配置日志

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # 配置信息
+import logging
 import redis
 
 
@@ -23,6 +24,9 @@ class Config(object):
     SESSION_REDIS = redis.StrictRedis(REDIS_HOST, REDIS_PORT)
     PERMANENT_SESSION_LIFETIME = 3600 * 24 * 2  # 设置session过期时间2天, 单位秒
 
+    # 默认的日志级别
+    LEVEL = logging.DEBUG
+
 
 # 开发模式
 class DevelopConfig(Config):
@@ -34,6 +38,7 @@ class ProductConfig(Config):
     # 生产环境的数据库,可以配置
     # 关闭调试模式
     DEBUG = False
+    LEVEL = logging.ERROR
 
 
 # 测试模式
--- info/__init__.py
+++ info/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 import redis
 from flask import Flask
 from flask.ext.session import Session
@@ -8,13 +11,16 @@ from config import config_dict
 
 # 定义redis
 redis_store = None
+# 创建SQLAlchemy对象
+db = SQLAlchemy()
 
 
 def create_app(env_name):
     app = Flask(__name__)
 
-    # 创建数据库对象，关联app
-    db = SQLAlchemy(app)
+    # 使用db,关联app
+    db.init_app(app)
+
     # 加载配置信息
     config = config_dict[env_name]
     app.config.from_object(config)
@@ -30,3 +36,17 @@ def create_app(env_name):
     Session(app)
 
     return app
+
+
+# 配置日志记录信息,就是为了方便记录程序运行过程
+def log_file(LEVEL):
+    # 设置日志的记录等级,大小关系: DEBUG<INFO<WARING<ERROR
+    logging.basicConfig(level=LEVEL)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
